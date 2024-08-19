092651a4c3cae4a74b1a71366b843d6f767a3656
Yoshida232 <saimoonyoshida1@gmail.com>
2024-08-19 12:25:21 +0600

    Update unsendMessage.js

--- Nayan/system/login/src/unsendMessage.js
+++ Nayan/system/login/src/unsendMessage.js
@@ -5,36 +5,42 @@ var log = require("npmlog");
 
 module.exports = function (defaultFuncs, api, ctx) {
   return function unsendMessage(messageID, callback) {
-    var resolveFunc = function () { };
-    var rejectFunc = function () { };
+    // Initialize promise resolve and reject functions
+    var resolveFunc, rejectFunc;
     var returnPromise = new Promise(function (resolve, reject) {
       resolveFunc = resolve;
       rejectFunc = reject;
     });
 
-    if (!callback) {
-      callback = function (err, friendList) {
-        if (err) return rejectFunc(err);
-        resolveFunc(friendList);
-      };
-    }
+    // Define a function to handle success or failure
+    var handleResponse = function (err, result) {
+      if (err) {
+        if (callback) callback(err); // Callback with error if provided
+        rejectFunc(err); // Reject the promise
+      } else {
+        if (callback) callback(null, result); // Callback with result if provided
+        resolveFunc(result); // Resolve the promise
+      }
+    };
 
     var form = {
       message_id: messageID
     };
 
+    // Perform the POST request and handle the response
     defaultFuncs
       .post("https://www.facebook.com/messaging/unsend_message/", ctx.jar, form)
       .then(utils.parseAndCheckLogin(ctx, defaultFuncs))
       .then(function (resData) {
-        if (resData.error) throw resData;
-        return callback();
+        if (resData.error) throw resData.error; // Throw if there is an error in the response
+        handleResponse(null, resData); // No error, handle success
       })
       .catch(function (err) {
         log.error("unsendMessage", err);
-        return callback(err);
+        handleResponse(err); // Handle error
       });
 
-    return returnPromise;
+    // Return the promise if no callback is provided
+    return callback ? undefined : returnPromise;
   };
 };
